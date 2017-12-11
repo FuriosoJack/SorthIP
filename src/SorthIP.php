7214741ee712ed39c3b60a76cfdf5f3e162bbfc5
FuriosoJack <furiosojuan0@gmail.com>
2017-12-11 15:42:42 -0500

    Se añade lanzamiento

--- src/SorthIP.php
+++ src/SorthIP.php
@@ -23,19 +23,94 @@
  */
 
 namespace FuriosoJack\SorthIP;
+use FuriosoJack\MrBinaryTree\MrBinaryTree;
+
 
 /**
- * Description of SorthIP
- *
+ * Ordena un listado de ip o subredes 
+ * 
+ * Es importante tener en cuenta que si existe en el arreglo varioas subredes o ip iguales 
+ * al final del procesamiento solo se vera reflejado una subred o ip de los repetidos ya que 
+ * a la hora de hacer el ordenamiento la ip o subred sera usada como key en un arreglo
+ * necesario para hacer el ordenamiento por que al existir el mismo key el nuevo valor reemplazara al anterior
+ * 
  * @author Juan Diaz - FuriosoJack <http://blog.furiosojack.com/> 
  */
 class SorthIP {
     
     protected $listAdresses;
     
+    protected $tree;
+
+
+    /**
+     * 
+     * @param array $list lista de direcciones ip o subnets
+     */
     public function __construct(array $list)
     {
         $this->listAdresses = $list;
+        $this->tree = new MrBinaryTree();
+        $this->buildTree();
+    }
+    
+    /**
+     * Convierte una ipaddress o subnet tipo string en un valor entero
+     * @param string $address
+     * @return string se retorna como estring para usarle luego como key
+     */   
+    protected function convertToInteger(string $address) : float
+    {
+        $resul = 0;
+        
+        //Se divide la ip por los octetos
+        $localArrayAdress = explode('.', $address);
+        
+        /*
+         * Se recorre cada seccion del array
+         * Cada octeto(item del arreglo) se multiplica por ( 255 elevalo al numero de octetos a la izquierda del octeto que se esta actualmente)
+         * Ejemplo: 192.168.0.1 =>  192 * 255^3 + 168 * 255^2 + 0 * 255^1 + 1 * 255^0 
+         * 
+         */
+        for ($i=0; $i < 4; $i++){
+            
+            $resul = $resul + ( $localArrayAdress[$i] * pow( 255, ( 4 - ( $i + 1) ) ) );
+           
+        }        
+        
+        return $resul;
+        
+    }
+    
+    /**
+     * Contrulle el arbol recorriendo el arreglo de direcciones y añadiendo los nodos al arbol
+     */
+    protected function buildTree()
+    {
+        foreach ($this->listAdresses as $address){
+            $keyAdress = $this->convertToInteger($address);
+            $this->tree->addNode($keyAdress, $address);            
+        }
+    }
+
+    /**
+     * retorna la lista de direcciones ordenadas de menor a mayor
+     * @return array
+     */
+    public function orderAsc(): array
+    {              
+       $this->tree->inDesc($this->tree->getRoot());     
+       return $this->tree->getLisOrderedList();      
+    }
+    
+    /**
+     * retorna la lista de direcciones ordenadas de mayor a menor
+     * @return array
+     */
+    public function orderDesc(): array
+    {
+        $this->tree->inDesc($this->tree->getRoot());
+        return $this->tree->getLisOrderedList();
     }
     
 }
